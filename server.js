e21fc59626fc01847344f982316ee6eb5835d186
Software-Today <nicoli.dev.430@gmail.com>
2022-09-19 18:46:39 -0700

    add models controllers

--- server.js
+++ server.js
@@ -15,6 +15,17 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
+const db = require("./app/models");
+db.mongoose.connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to the database!");
+}).catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+});
+
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to application." });
