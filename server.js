4e43fc0bb402db1f1bff8cf07c2084f925ee118a
Software-Today <nicoli.dev.430@gmail.com>
2022-09-19 19:14:24 -0700

    add routes

--- server.js
+++ server.js
@@ -26,10 +26,7 @@ db.mongoose.connect(db.url, {
     process.exit();
 });
 
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to application." });
-});
+require("./app/routes/tutorial.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
